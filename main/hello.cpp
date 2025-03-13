b88e148da378800bb0bd7fc46fae10380ffe1c19
Kaushik Bhat <128929833+kaushik-bhat@users.noreply.github.com>
2025-03-13 15:49:22 +0530

    Changes to main function

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, Welcome to Kaushik (PES1UG22CS279) World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have made changes to the cpp file:) \n";
   return 0;
 }
